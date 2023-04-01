4fa983ce860ee65b64b49cd1b0ddf4eb9fe8de30
Willgraves1 <118127287+Willgraves1@users.noreply.github.com>
2023-04-01 23:48:28 +0100

    Update chatbot.js

--- chat/chatbot.js
+++ chat/chatbot.js
@@ -1,3 +1,5 @@
+import phrases from './phrases.js';
+
 const chatbot = () => {
   let name = prompt("Hello! What's your name?");
   let message = `Nice to meet you, ${name}! How can I help you today?`;
@@ -16,23 +18,20 @@ const chatbot = () => {
   document.querySelector('.btn').addEventListener('click', () => {
     let userInput = document.querySelector('input').value;
     addBubble(userInput, 'user');
-    switch (userInput.toLowerCase()) {
-      case "hi":
-      case "hello":
-        message = "Hi there! How can I assist you today?";
+
+    let responseFound = false;
+    for (let i = 0; i < phrases.length; i++) {
+      if (phrases[i].input.includes(userInput.toLowerCase())) {
+        message = phrases[i].output;
+        responseFound = true;
         break;
-      case "how are you":
-        message = "I'm just a computer program, so I'm always doing well. Thanks for asking!";
-        break;
-      case "what's your name":
-        message = "My name is Chatbot!";
-        break;
-      case "what can you do":
-        message = "I can answer questions, give advice, and chat with you. Just let me know what you need!";
-        break;
-      default:
-        message = "I'm sorry, I didn't understand that. Could you please rephrase your question?";
+      }
     }
+
+    if (!responseFound) {
+      message = "I'm sorry, I didn't understand that. Could you please rephrase your question?";
+    }
+
     addBubble(message, 'bot');
     document.querySelector('input').value = '';
   });
