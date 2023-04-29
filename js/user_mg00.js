4444fd8120a5f1e187f1baab28ab677beea924b5
willgraves1 <williamggraves@icloud.com>
2023-04-29 15:56:59 +0100

    edit

---
+++ js/user_mg00.js
@@ -0,0 +1,16 @@
+var user = {
+    firstName:"Matt",
+    lastName:"Graves"
+};
+
+var dashboard = {
+    errors:"0",
+    issues:"0",
+    chat:"0",
+    bounce:"",
+}
+
+    var ads = {
+        advertiser:"NordVPN",
+        message:"View the web without a worry about your security"
+    }
