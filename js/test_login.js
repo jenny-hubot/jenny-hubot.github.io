054946d79555cfc6ad43106a96062a9bab1ee114
willgraves1 <williamggraves@icloud.com>
2023-06-03 16:42:25 +0100

    edit

---
+++ js/test_login.js
@@ -0,0 +1,35 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "AGraves@sawi.online" && password == "Password1?"){
+alert ("Login successfully");
+window.location = "../testing/agraves@sawi.online/index.html"; // Redirecting to other page.
+}
+if ( username == "SGsysadmin" && password == "WDGwieo48(92"){
+    alert ("Login successfully");
+    window.location = "../users/waitSpace2.html";
+}
+if ( username == "MattGraves00" && password == "sakrod-rahvl0-vakniz"){
+    alert ("Login successfully");
+    window.location = "../user/waitSpace2.html";
+return false;
+}
+if ( username == "HGsysadminBE" && password == "R2D2/BB8"){
+    alert ("Login successfully");
+    window.location = "https://benny-hubot.github.io/login.html";
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
